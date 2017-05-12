fbae85910510b7ec778a4372826ca9446aa50699
Patrick Edson <pedson@leapscientific.com>
2017-05-11 20:08:03 -0400

    Export all namespaces for convenience

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,23 @@ export {IMouseStrain} from "./sample/MouseStrain";
 export {INeuron} from "./sample/Neuron";
 export {IRegistrationTransform} from "./sample/RegistrationTransform";
 export {ISample} from "./sample/Sample";
+
+import {BrainArea} from "./sample/brainArea";
+import {Fluorophore} from "./sample/fluorophore";
+import {Injection} from "./sample/injection";
+import {InjectionVirus} from "./sample/InjectionVirus";
+import {MouseStrain} from "./sample/mousestrain";
+import {Neuron} from "./sample/neuron";
+import {RegistrationTransform} from "./sample/registrationTransform";
+import {Sample} from "./sample/sample";
+
+export const AllModels = [
+    BrainArea,
+    Fluorophore,
+    Injection,
+    InjectionVirus,
+    MouseStrain,
+    Neuron,
+    RegistrationTransform,
+    Sample
+];
